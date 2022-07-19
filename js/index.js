cf15a754154fa4c89b57d53591e42c6f76e31a03
Jaytee <Padilla.Jaytee@gmail.com>
2022-07-19 00:04:30 -0700

    Setup basic fetch request to API

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
+// modules
 import 'dotenv/config';
+
+// HTML elements
+const ipAddressEl = document.getElementById('ip-address-input');
+const searchButtonEl = document.querySelector('.search-btn');
+
+// vars
+const ApiKey = process.env.API_KEY;
+
+const getIpGeolocation = async () => {
+  const ipAddress = ipAddressEl.value;
+
+  const response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${ApiKey}&ipAddress=${ipAddress}`)
+    .then(res => {
+      if (!res.ok) {
+        const message = `An error has occurred: ${res.status} ${res.statusText}`
+        throw new Error(message);
+      }
+      
+      const data = res.json()
+      return data;
+    })
+
+  console.log(response);
+  return response;
+}
+
+searchButtonEl.addEventListener('click', getIpGeolocation);
